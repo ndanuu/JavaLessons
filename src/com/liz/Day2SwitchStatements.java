8f8e28d8029dcecd060f0cd6e82f21d05571a238
ndanumbuvi <lisbethndanu45@gmiail.com>
2018-12-07 09:55:50 +0300

    first commit

---
+++ src/com/liz/Day2SwitchStatements.java
@@ -0,0 +1,31 @@
+package com.liz;
+
+public class Day2SwitchStatements {
+    public static void main(String[] args) {
+        //Switch Case Statements
+        int x;
+        x=8;
+        switch (x){
+            case 10:
+            case 9:
+            case 8:
+                System.out.println("The answer is 10");
+                break;
+
+            case 11:
+                System.out.println("The answer is 11");
+                break;
+
+            case 12:
+                System.out.println("The answer is 12");
+                break;
+
+                default:
+                    System.out.println("Hapo uko nje brathe");
+
+
+        }
+
+
+    }
+}
