306bc96962211ac7d6e26201085a6683c5df2c2e
ndanumbuvi <lisbethndanu45@gmiail.com>
2018-12-07 09:41:18 +0300

    first commit

--- src/com/liz/Day1IfStatements.java
+++ src/com/liz/Day1IfStatements.java
@@ -1,4 +1,56 @@
 package com.liz;
 
 public class Day1IfStatements {
+    public static void main(String[] args) {
+        double weight=58;
+        double height=1.67;
+        double bmi=weight/(height*height);
+
+        //System.out.println(bmi);
+        if (bmi<15)
+        {
+            System.out.println("Very Severely Underweight");
+        }
+        else if (bmi<16)
+        {
+            System.out.println("Severely Underweight");
+        }
+        else if (bmi<18.5)
+        {
+            System.out.println("Underweight");
+        }
+        else if (bmi<25)
+        {
+            System.out.println("Normal(healthy weight)");
+
+        }
+        else if (bmi<30)
+        {
+            System.out.println("Overweightt");
+        }
+        else if (bmi<35){
+            System.out.println("Obese Class I(Moderately Obese)");
+        }
+        else if (bmi<40)
+        {
+            System.out.println("Obese Class II(Severely Obese)");
+        }
+        else if (bmi<45)
+        {
+            System.out.println("Obese class III(Very Severely Obese)");
+        }
+        else if (bmi<50)
+        {
+            System.out.println("Obese class IV (Morbidly Obese)");
+        }
+        else if (bmi<60)
+        {
+            System.out.println("Obese Class V(Suoer Obese)");
+        }
+        else{
+            System.out.println("Obese Class VI(Hyper Obese)");
+        }
+
+
+    }
 }
---
+++ src/com/liz/Day1Variables.java
@@ -0,0 +1,20 @@
+package com.liz;
+
+public class Day1Variables {
+    public static void main(String[] args) {
+//control space will make intelij to autosuggest
+        int age =70;
+        double weight=65.5;
+        String names="John Mark";
+        boolean isAlive=true;
+        double height=10;
+        System.out.println(" "+ age +" "+weight);
+        System.out.println(names+" "+weight);
+
+        float distance=100.99f;
+        short r=67;
+        long l=10000000;
+        char s=' ';
+        System.out.println(distance);
+    }
+}
